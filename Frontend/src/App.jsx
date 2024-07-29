97016bd43e45c49c60d02d9713390e526a6c36e3
Pratik <pratikspatil3040@gmail.com>
2024-07-29 05:27:48 -0700

    Add Banner

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBAr from './Components/NavBAr'
 import Banner from './Components/Banner'
+import Footer from './Components/Footer'
 
 function App() {
   return (
     <>
       <NavBAr />
       <Banner />
+      <Footer />
     </>
   )
 }
