1ef4b7ce633df6b8eff13c08d25409bf3ebc2ffb
Pratik <pratikspatil3040@gmail.com>
2024-07-29 04:20:25 -0700

    Initial Commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBAr from './Components/NavBAr'
+import Banner from './Components/Banner'
+
+function App() {
+  return (
+    <>
+      <NavBAr />
+      <Banner />
+    </>
+  )
+}
+
+export default App
